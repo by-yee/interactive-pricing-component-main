44a7e31090550095f4b889c24d43a9ba19d25072
bengyee <bengyee94@gmail.com>
2021-08-05 21:17:13 +0800

    Interactive Pricing Component
    - Move the repeated slider code into @mixin
    - Add animation to the button when hover
    - Fixed the slider progress for Chrome browser, need to use reactjs to calculate the percentage
       of the backgroundImage linear gradient.

--- src/js/Main.js
+++ src/js/Main.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react'
 class Main extends Component {
   constructor(props) {
     super()
+    this.progress = { 
+      1: { percent: "0%" }, 
+      2: { percent: "25%" }, 
+      3: { percent: "50%" },
+      4: { percent: "75%" },
+      5: { percent: "100%" } 
+    }
   }
 
   render() {
+    //Slider progress bar for Chrome browser
+    const percent = this.progress[this.props.RangeSlider]["percent"]
+    const sliderProgress = {
+      backgroundImage: `-webkit-gradient(linear, left top, right top, color-stop(${percent}, hsl(174, 77%, 80%)), color-stop(${percent}, hsl(224, 65%, 95%)))`
+    };
+
     return(
       <div className="main__flex">
         <div className="main__pageview">
@@ -22,6 +35,7 @@ class Main extends Component {
             min="1" 
             max="5" 
             value={this.props.RangeSlider}
+            style={sliderProgress}
             onChange={(e) => this.props.handleRange(e)}
           />
         </div>
